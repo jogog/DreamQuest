fe3a7b149d81f0a3da12033a9d660f39ff03a7e7
jogog <jogog007@mail.ru>
2019-12-07 11:33:55 +0300

    Init commit

---
+++ src/main/java/com/dreamTeam/dreamQuest/Main.java
@@ -0,0 +1,12 @@
+package com.dreamTeam.dreamQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
